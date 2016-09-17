012454057facd26a391958e9ea5c79d6b1e79a73
Vlad <ogay.vlad@gmail.com>
2016-09-17 11:55:24 +0300

    less

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 // styles
 require('bootstrap/less/bootstrap.less');
 require('../font-awesome/css/font-awesome.css');
-require('../css/carousel.less');
-require('../css/fonts.less');
-require('../css/main.less');
+require('../less/carousel.less');
+require('../less/fonts.less');
+require('../less/main.less');
 
 // scripts
 require('bootstrap/dist/js/npm');
