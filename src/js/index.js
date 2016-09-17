d770533929a7f2622490634003da3f50e9a8727b
Vlad <ogay.vlad@gmail.com>
2016-09-17 12:08:11 +0300

    cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
 // styles
 require('bootstrap/less/bootstrap.less');
 require('../font-awesome/css/font-awesome.css');
-require('../less/carousel.less');
-require('../less/fonts.less');
 require('../less/main.less');
 
 // scripts
