e32fff649e19b42e717415f60f725cace1e6b32b
NTer <NTer>
2016-09-21 15:47:22 +0300

    js nav

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,21 @@ require('../less/main.less');
 
 // scripts
 require('bootstrap/dist/js/npm');
+
+$('.carousel').carousel({interval: 2000})
+
+$(document).ready(function(){
+
+    $(".nav").on("click","a", function (event) {
+
+        event.preventDefault();
+
+        var id  = $(this).attr('href'),
+
+            top = $(id).offset().top;
+
+        $('body,html').animate({scrollTop: top}, 1500);
+
+    });
+
+});
