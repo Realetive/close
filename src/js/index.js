a3d30953ac7b119755f1bc2b95b42710e95bcffb
Vlad <ogay.vlad@gmail.com>
2016-09-22 13:49:39 +0300

    magnific-popup

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 // styles
 require('bootstrap/less/bootstrap.less');
+require('magnific-popup/dist/magnific-popup.css');
 require('../font-awesome/css/font-awesome.css');
 require('../less/main.less');
 
 // scripts
 require('bootstrap/dist/js/npm');
+require('magnific-popup/dist/jquery.magnific-popup.js');
 
 $('.carousel').carousel({interval: false})
 
@@ -22,4 +24,8 @@ $(document).ready(function(){
 
     });
 
+    $('.inf').magnificPopup({
+        type:'inline',
+        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });
 });
