a4e1c4d2269aeaba4d60ed126d7b01f5be9c21ce
Vlad <ogay.vlad@gmail.com>
2016-09-22 17:12:56 +0300

    reorganized program

--- src/js/data.js
+++ src/js/data.js
@@ -1,34 +1,33 @@
-module.exports = [
-    {
+module.exports = {
+    sverchok: {
         title: "&laquo;Сказка о семейном счастье, или Сверчок за очагом&raquo;",
         author: "Чарльз Диккенс",
         imgHolder: "holder.js/900x500/auto/#777:#7a7a7a/text:First slide",
-        imgCover: require("../img/sverchok/cover.jpg"),
+        imgCover: require("../program/sverchok/img/cover.jpg"),
         state: "active"
     },
-    {
+    nepostoyanstvo: {
         title: "&laquo;Двойное <br/>непостоянство&raquo;",
         author: "Пьер Карле де Шамблен де Мариво",
         imgHolder: "holder.js/900x500/auto/#666:#6a6a6a/text:Second slide",
-        imgCover: require("../img/nepostoyanstvo/cover.jpg"),
+        imgCover: require("../program/nepostoyanstvo/img/cover.jpg"),
     },
-    {
+    wolf: {
         title: "&laquo;Хочу быть волком&raquo;",
         author: "Беттина Вегенаст",
         imgHolder: "holder.js/900x500/auto/#555:#5a5a5a/text:Third slide",
-        imgCover: require("../img/wolf/cover.jpg"),
+        imgCover: require("../program/wolf/img/cover.jpg"),
     },
-    {
+    belyenochi: {
         title: "&laquo;Белые ночи&raquo;",
         author: "Ф.М. Достоевский",
         imgHolder: "holder.js/900x500/auto/#555:#5a5a5a/text:Third slide",
-        imgCover: require("../img/belyenochi/cover.jpg"),
+        imgCover: require("../program/belyenochi/img/cover.jpg"),
     },
-    {
+    derevnya: {
         title: "&laquo;Месяц в деревне&raquo;",
         author: "И.С.Тургенев",
         imgHolder: "holder.js/900x500/auto/#555:#5a5a5a/text:Third slide",
-        imgCover: require("../img/derevnya/cover.jpg"),
+        imgCover: require("../program/derevnya/img/cover.jpg"),
     },
-
-]
+}
